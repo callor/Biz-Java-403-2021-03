691dc75d2a7945904f8836261fced26232eee374
callor <callor@callor.com>
2021-03-17 10:03:59 +0900

    연산복습

---
+++ Java_000_Hello/src/com/callor/hello/Numbers01.java
@@ -0,0 +1,36 @@
+package com.callor.hello;
+
+/*
+ * ============================
+ * 33 + 2 =
+ * 33 - 2 =
+ * 33 * 2 =
+ * 33 / 2 =
+ * 33 % 2 =
+ * ============================
+ */
+public class Numbers01 {
+	public static void main(String[] args) {
+		
+		System.out.println("=====================");
+		System.out.println("33 + 2 = " + (33 + 2));
+		System.out.println("33 - 2 = " + (33 - 2));
+		System.out.println("33 x 2 = " + (33 * 2));
+		System.out.println("33 / 2 = " + (33 / 2));
+		System.out.println("33 % 2 = " + (33 % 2));
+		System.out.println("=======================");
+		
+		System.out.println("=======================");
+		System.out.print("33 + 2 = ");
+		System.out.println(33 + 2);
+		System.out.print("33 - 2 = ");
+		System.out.println(33 - 2);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
