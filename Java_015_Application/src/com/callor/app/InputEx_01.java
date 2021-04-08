83d1f383f23bc0b51a22a2c63200b4da75f595b3
callor <callor@callor.com>
2021-04-08 16:55:37 +0900

    Score

--- Java_015_Application/src/com/callor/app/InputEx_01.java
+++ Java_015_Application/src/com/callor/app/InputEx_01.java
@@ -7,14 +7,10 @@ public class InputEx_01 {
 	
 	public static void main(String[] args) {
 		
-		InputService isV1 
-			= new InputServiceImplV1();
+		InputService isV1 = new InputServiceImplV1();
 		isV1.inputValue("새우깡");
 		
 		System.out.println(isV1.toString());
-		
-		
-		
 		
 	}
 }
